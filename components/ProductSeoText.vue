96d5b85e976767b4832a28a605e7f1ab98ee9c76
schlafsack24-net <schlafsack24.net@surrealnetworks.io>
2023-02-17 16:03:26 +0100

    productseotext.vue component forgotte :(

---
+++ components/ProductSeoText.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-if="seoData && seoData.seo && seoData.seo.texts">
+        <div v-if="seoData && seoData.seo && seoData.seo.texts">
+            <div v-for="(text, index) in seoData.seo.texts" :key="index">
+                <div class="mb-3 mt-5 h5">
+                    {{ text.title }}
+                </div>
+                <p class="mb-4">
+                    {{ text.text }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "productSeoText",
+    props: {
+        seoData: Object,
+    },
+};
+</script>
